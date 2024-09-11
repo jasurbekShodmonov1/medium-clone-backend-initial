8c5f2c9c9c34a50539014ec38347c766a92b5212
jasurbekShodmonov1 <jasurshodmonov21020@gmail.com>
2024-09-11 12:09:02 +0500

    Medium app

--- users/models.py
+++ users/models.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import os
+import uuid
 
+def file_upload(instance, filename):
+    """ This function is used to upload the user's avatar. """
+    ext = filename.split('.')[-1]
+    filename = f'{instance.username}.{ext}'
+    return os.path.join('users/avatars/', filename)
 
 class CustomUser(AbstractUser):
     """  This model represents a custom user. """
+    avatar = models.ImageField(upload_to=file_upload, blank=True)
     middle_name = models.CharField(max_length=30, blank=True, null=True)
 
     class Meta:
